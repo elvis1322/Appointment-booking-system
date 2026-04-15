6b99c0ce47860e67657c060778b2733915cd94f3
elvis1322 <elviszogaj12@gmail.com>
2026-04-16 00:55:31 +0200

    Konfigurimi i databazes dhe shtimi i migrimev dhe krijimi i datazes

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33643b22dec48e37560f7b37d09b2b6cbe3190ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1dec8f890e6c084ddec00e97142f61a75676b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Persistence/Data/DataContext.cs
+++ Persistence/Data/DataContext.cs
@@ -1,7 +1,7 @@
 using Domain.Entities;
 using Domain.Entities.Constants;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Http; // E rëndësishme
+
 using System.Security.Claims;    // Shto këtë për ClaimTypes
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +12,22 @@ namespace Persistence.Data;
 
 public class DataContext : DbContext
 {
-    // 1. Deklaro fushën private
+    // public DataContext(DbContextOptions<DataContext> options)
+       public DataContext(DbContextOptions<DataContext> options) 
+        : base(options)
+    {
+       
+    }
    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<RolePermission> RolePermissions { get; set; }
+    public DbSet<AuditLog> AuditLogs { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
