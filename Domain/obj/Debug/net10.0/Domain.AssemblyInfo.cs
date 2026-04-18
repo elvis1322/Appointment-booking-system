56eb32e7dcaaa6004850e840dbd3415af6eb76c3
elvis1322 <elviszogaj12@gmail.com>
2026-04-18 02:06:31 +0200

    Configurimi dhe shtimi i repositories per refresh token dhe berja update e Readme

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1dec8f890e6c084ddec00e97142f61a75676b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f46e793e9f959c5c1297213ee3dc3fd2715ad13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
