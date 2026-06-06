594d8de70f9ce77107c445af4db48aae5753dad2
Rina <r33bashota@gmail.com>
2026-06-06 19:32:49 +0200

    Integrimi i Invoice Repositories

---
+++ Persistence/Repositories/Invoice/InvoiceRepository.cs
@@ -0,0 +1,35 @@
+using Domain.Entities;
+using Persistence.Data;
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Repositories;
+public class InvoiceRepository : IInvoiceRepository
+    {
+        private readonly DataContext _context;
+
+        public InvoiceRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Invoice invoice)
+        {
+            await _context.Invoices.AddAsync(invoice);
+        }
+
+        public async Task<Invoice?> GetByIdAsync(Guid id)
+        {
+            return await _context.Invoices.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<Invoice?> GetByOrderIdAsync(Guid orderId)
+        {
+            return await _context.Invoices.FirstOrDefaultAsync(i => i.OrderId == orderId);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
